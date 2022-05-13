2ff1ede8c6d3a82226c0d0968e491282e4ba1c85
Marina-78 <nachmarina@yandex.ru>
2022-05-13 17:37:25 +0300

    v_1_1_3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl user = new UserServiceImpl();
+        user.createUsersTable();
+
+        user.saveUser("Ivan", "Ivanov", (byte) 20);
+        user.saveUser("Petr", "Petrov", (byte) 25);
+        user.saveUser("Stepan", "Stepanov", (byte) 31);
+        user.saveUser("Anton", "Antonov", (byte) 38);
+
+        for (User us : user.getAllUsers()) {
+            System.out.println("User с именем – " + us.getName() + " добавлен в базу данных");
+
+        }
+        System.out.print(user.getAllUsers());
+
+        user.cleanUsersTable();
+        user.dropUsersTable();
     }
 }
